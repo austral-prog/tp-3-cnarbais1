fdf8ae35fcbc1c784e50ade084609188c94184c5
cnarbais1 <cnarbais1@mail.austral.edu.ar>
2025-04-11 12:49:19 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,5 @@
-  def slice_simple():
+def slice_simple():
     texto = "Awesome"
-    print(texto[:3].lower())
-    print(texto[2:5].lower())
-    print(texto[:5].lower() + texto[5:].lower())
+    print (texto[:3].lower())
+    print (texto[2:5].lower())
+    print (texto[:5].lower() + texto[5:].lower())
